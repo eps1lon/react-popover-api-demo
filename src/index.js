8da1af498238c3d117334e26563bc38d0dd21913
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2024-05-01 12:17:38 +0000

    Polish

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import { StrictMode, version } from "react";
 import { createRoot } from "react-dom/client";
 
 const rootElement = document.getElementById("root");
@@ -6,32 +6,55 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <details onToggle={event => { console.log('details toggle', event) }}>
+    <p>React: {version}</p>
+    <details
+      onToggle={(event) => {
+        console.log("details toggle", event);
+      }}
+    >
       <summary>Summary</summary>
       <div>Details</div>
     </details>
-    <button popoverTargetElement="mypopover">Toggle the popover (popoverTargetElement)</button>
-    <button popoverTargetElement="mypopover" popoverTargetAction="show">Show the popover</button>
-    <button popoverTargetElement="mypopover" popoverTargetAction="hide">Hide the popover</button>
-    <button popoverTarget="mypopover">Show the popover  (warns about using "popoverTargetElement"</button>
-    <div id="mypopover" popover="auto" onToggle={event => {
-      console.log(event)
-    }} onBeforeToggle={event => {
-      console.log(event)
-    }}>
+    <button popoverTargetElement="mypopover">
+      Toggle the popover (popoverTargetElement)
+    </button>
+    <button popoverTargetElement="mypopover" popoverTargetAction="show">
+      Show the popover
+    </button>
+    <button popoverTargetElement="mypopover" popoverTargetAction="hide">
+      Hide the popover
+    </button>
+    <button popoverTarget="mypopover">
+      Show the popover (warns about using "popoverTargetElement"
+    </button>
+    <div
+      id="mypopover"
+      popover="auto"
+      onToggle={(event) => {
+        console.log(event);
+      }}
+      onBeforeToggle={(event) => {
+        console.log(event);
+      }}
+    >
       Popover content
     </div>
 
-    <button popoverTargetElement="mypopover-cancelled">Toggle the popover (but the popover cancels that via preventDefault)</button>
-    <div id="mypopover-cancelled" popover="auto" onToggle={event => {
-      console.log(event)
-    }} onBeforeToggle={event => {
-      console.log(event)
-      event.preventDefault()
-    }}>
+    <button popoverTargetElement="mypopover-cancelled">
+      Toggle the popover (but the popover cancels that via preventDefault)
+    </button>
+    <div
+      id="mypopover-cancelled"
+      popover="auto"
+      onToggle={(event) => {
+        console.log(event);
+      }}
+      onBeforeToggle={(event) => {
+        console.log(event);
+        event.preventDefault();
+      }}
+    >
       Popover content
     </div>
-  </StrictMode>
+  </StrictMode>,
 );
-
-
